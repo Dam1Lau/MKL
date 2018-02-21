6df76eff702f6390968f31fc900e19e3e67afa1a
dam1 <dam1@1DAM-04.ies.quevedo>
2018-02-21 13:56:48 +0100

    Comprobando que donde quieras mover no exista una pieza tuya

--- src/ajedrez2/Juego.java
+++ src/ajedrez2/Juego.java
@@ -67,17 +67,18 @@ public class Juego {
      * Preguntará al usuario el movimiento que quiere hacer (su jugada) y eso
      * hará que se genere un movimiento.
      *
-     * El usuario solo puede introducir una jugada de esta forma
-     * "A1B1" donde las letras son las columnas del movimiento, y los números
-     * las filas del movimiento generado. Si la jugada genera un movimiento no
-     * válido, le debe preguntar de nuevo por el movimiento que desea
-     * introducir. Además, debemos hacer todas las comprobaciones necesarias para estar seguros de que el movimiento es
-     * correcto antes de generar el movimiento en el ultimo paso.
+     * El usuario solo puede introducir una jugada de esta forma "A1B1" donde
+     * las letras son las columnas del movimiento, y los números las filas del
+     * movimiento generado. Si la jugada genera un movimiento no válido, le debe
+     * preguntar de nuevo por el movimiento que desea introducir. Además,
+     * debemos hacer todas las comprobaciones necesarias para estar seguros de
+     * que el movimiento es correcto antes de devolver el movimiento en el
+     * ultimo paso.
+     *
      * @param tablero
-     * @return movimiento válido tras las comprobaciones 
+     * @return movimiento válido tras las comprobaciones
      */
-    
-    public Movimiento meterJugada(Tablero tablero) {  
+    public Movimiento meterJugada(Tablero tablero) {
         boolean noValido = true;
         Movimiento movi = new Movimiento();
         Scanner lector = new Scanner(System.in);
@@ -88,17 +89,21 @@ public class Juego {
                 System.out.println("Jugada inválida, introduce una jugada de cuatro caracteres en éste orden: A1C2");
             } else if (jugada.charAt(0) < 'A' || jugada.charAt(0) > 'H' || jugada.charAt(2) < 'A' || jugada.charAt(2) > 'H') {
                 System.out.println("Jugada incorrecta. Ha introducido una letra inválida o en posición errónea.");
-            } else if ((int)jugada.charAt(1) < 49   
+            } else if ((int) jugada.charAt(1) < 49
                     || jugada.charAt(1) > '8'
                     || jugada.charAt(3) < '1'
                     || jugada.charAt(3) > '8') {
                 System.out.println("Jugada incorrecta. Ha introducido un número inválido o en posición incorrecta.");
-            } else if(tablero.hayPieza((int)jugada.charAt(1)-49,(int)jugada.charAt(0)-65) == false){
+            } else if (tablero.hayPieza((int) jugada.charAt(1) - 49, (int) jugada.charAt(0) - 65) == false) {
                 System.out.println("No hay pieza en esa casilla.");
-            }else if(tablero.buscarPieza((int)jugada.charAt(1)-49,(int)jugada.charAt(0)-65).getColor().charAt(0) != this.toString().charAt(0)){  //Aqui da fallo del tamaño del array si pongo 48 vale para la fila 0 pero no para la 8
-                System.out.println("Esa pieza no es de tu color.");
-            }else {//Validar si hay pieza en pos incial color turno, si hay pieza pos final y el color es el mismo turno
-                movi.setPosInicial(new Posicion(jugada.charAt(1) - 49,jugada.charAt(0) -65));
+            } else if (tablero.buscarPieza((int) jugada.charAt(1) - 49, (int) jugada.charAt(0) - 65).getColor().charAt(0) != this.toString().charAt(0)) {
+                System.out.println("Esa pieza no es de tu color. No es tu turno.");
+
+            } else if (tablero.hayPieza(jugada.charAt(3) - 49, jugada.charAt(2) - 65) == true 
+                    && tablero.buscarPieza((int) jugada.charAt(1) - 49, (int) jugada.charAt(0) - 65).getColor().charAt(0) == this.toString().charAt(0)){
+                System.out.println("Hay una pieza de tu color en esa posición.");
+            } else {//Validar si hay pieza en pos incial color turno, si hay pieza pos final y el color es el mismo turno
+                movi.setPosInicial(new Posicion(jugada.charAt(1) - 49, jugada.charAt(0) - 65));
                 movi.setPosFinal(new Posicion(jugada.charAt(3) - 49, jugada.charAt(2) - 65));
                 noValido = false;
             }
