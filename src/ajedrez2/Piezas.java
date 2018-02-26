29e29f1b9205924c9f66f996ca9c46c0f5ed755d
dam1 <dam1@1DAM-04.ies.quevedo>
2018-02-26 11:13:14 +0100

    Añadido rey movimiento e ya

--- src/ajedrez2/Piezas.java
+++ src/ajedrez2/Piezas.java
@@ -54,16 +54,25 @@ public abstract class  Piezas {
     
     // Métodos
     /**
-     * Método por el que cada pieza nos dirá si puede moverse en el tablero dependiendo de su forma de moverse
-     * y de si tiene otras piezas a su alrededor. Método abstracto que cambiará en cada clase que herede de pieza.
+     * Método por el que cada pieza nos dirá si puede moverse en el tablero dependiendo de su forma de moverse. 
+     * Método abstracto que cambiará en cada clase que herede de pieza.
+     * @param movimiento introducido por el usuario y que queremos saber si es correcto según el tipo de pieza 
+     * que movamos. El método es implementado por cada pieza individualmente.
      * @return true = si se puede mover, false = si tiene otras piezas bloqueando su movimiento.
      */
     public abstract boolean puedeMoverse(Movimiento movimiento);
+    /**
+     * Pintar pieza: Cada pieza devuelve un 
+     * @return 
+     */
     public abstract String pintarPieza();
 
-    
+    /**
+     * Método que devuelve un string con los atributos de la pieza.
+     * @return string con los atributos de la pieza: nombre, color y su valor en puntos
+     */
     @Override
     public String toString() {
-        return "Pieza{" + this.getClass().getSimpleName() + " " + color + '}';
+        return "Pieza" + this.getClass().getSimpleName() + " " + color + " y vale" + puntos + "punto/s";
     }  
 }
